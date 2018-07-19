f772fa11ff5cb360abec20f77028845d9c4d7f94
condal <conall.daly@insight-centre.org>
2018-07-19 09:48:38 +0100

    Adding Fantasy Overlord data

---
+++ scrape.py
@@ -0,0 +1,4 @@
+https://fantasyoverlord.com/FPL/History?file=FPL16-GW5
+https://fantasyoverlord.com/FPL/History?file=FPL16-GW6
+https://fantasyoverlord.com/FPL/History?file=FPL16-GW37
+https://fantasyoverlord.com/FPL/History?file=FPL18-GW0
